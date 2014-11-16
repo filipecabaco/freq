b5bf842e7e49b5cef9a0e168d3558b979cbba968
Filipe Cabaco <filipecabaco@gmail.com>
2014-11-16 16:54:21 +0000

    Added comments

--- src/main/scala/com/freq/core/Global.scala
+++ src/main/scala/com/freq/core/Global.scala
@@ -20,29 +20,38 @@ object Global extends App{
   val output = "Destination %s with Id %d:"
   println(s"Started with ids $ids" )
   executeWithFuture
-
-  def executeWithFuture = {
-    val futures = ids.map{id=>Future(Tools.getDestInfo(id))}
-    futures.map( _ onComplete{
-      case Success(dest) => {
-        val res = showWordCount(Tools.getAllHotelDescriptions(dest))
+/**
+ * Executes the word search wrapped in a Future
+ */
+ def executeWithFuture = {
+  val futures = ids.map{id=>Future(Tools.getDestInfo(id))}
+  futures.map( _ onComplete{
+    case Success(dest) => {
+      val res = getOrderedWordCount(Tools.getAllHotelDescriptions(dest).mkString)
+      if(res.length > 0){
         showOutput(dest,res)
       }
-      case Failure (t) => println("Error thrown - %s".format(t.getClass().getSimpleName()))
-      })
-  }
-
-  private def showWordCount(descritpions : List[String]) : Seq[(String,Int)] = {
-    val words = new Regex(pttr,"g") findAllIn descritpions.mkString
-    var res = collection.mutable.Map[String,Int]().withDefaultValue(0)
-
-    for(w<- words) {
-      res.update(w,(res(w)+1))
     }
-    res.toSeq.sortBy(_._2).reverse.toSeq
-  }
+    case Failure (t) => println("Error thrown - %s".format(t.getClass().getSimpleName()))
+    })
+}
+/**
+ * Counts the word occurence for a given String, returning a sequence of tuples order by word count
+ */
+ private def getOrderedWordCount(text : String) : Seq[(String,Int)] = {
+  val words = new Regex(pttr,"g") findAllIn text
+  var res = collection.mutable.Map[String,Int]().withDefaultValue(0)
 
-  private def showOutput(dest:Destination,seq:Seq[(String,Int)]){
+  for(w<- words) {
+    res.update(w,(res(w)+1))
+  }
+  res.toSeq.sortBy(_._2).reverse.toSeq
+}
+
+/**
+ * Prints out the full output of one operation
+ */
+ private def showOutput(dest:Destination,seq:Seq[(String,Int)]){
     //To ensure the output is shown correctly
     this.synchronized {
       println(output.format(dest.name,dest.id))
@@ -50,12 +59,18 @@ object Global extends App{
     }
   }
 }
-
-object Tools{
+/**
+ * Simple tools used in the main code
+ */
+ object Tools{
   //URL to get information of destionation
   val destinationInfoUrl = "http://concierge.top10.com/v1/destinations/%s"
   val hotelInfoUrl = "http://concierge.top10.com/v1/destinations/%s/hotels?hotel_attrs=description&count=20"
-  def getDestInfo(destId:String) : Destination = {
+
+  /**
+   * Retrieves information of the Destination based on his ID
+   */
+   def getDestInfo(destId:String) : Destination = {
     val obj = getJsonFromUrl(destinationInfoUrl.format(destId))
 
     //Extract name from JSON
@@ -64,21 +79,25 @@ object Tools{
       JField("id",JString(id)) <- o
       if id == destId
       JField("name",JString(n)) <- o
-    } yield n
+      } yield n
 
-    new Destination(name.mkString,destId.toInt)
-  }
-
-  def getAllHotelDescriptions(dest : Destination) : List[String] = {
+      new Destination(name.mkString,destId.toInt)
+    }
+  /**
+   * Retrieves all the descriptions for the hotels in the given Destination
+   */
+   def getAllHotelDescriptions(dest : Destination) : List[String] = {
     val obj = getJsonFromUrl(hotelInfoUrl.format(dest.id))
     val descritpions = for {
       JObject(o) <- obj
       JField("text",JString(d)) <- o
-    } yield d
-    descritpions
-  }
-
-  private def getJsonFromUrl(url:String) : JValue = {
+      } yield d
+      descritpions
+    }
+  /**
+   * Retrieve JSON from given URL
+   */
+   private def getJsonFromUrl(url:String) : JValue = {
     parse(scala.io.Source.fromURL(url).mkString)
   }
 }
