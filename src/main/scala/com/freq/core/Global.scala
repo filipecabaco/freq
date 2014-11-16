c66165940d407603671fb4f981914d35b7b5a7e8
Filipe Cabaco <filipecabaco@gmail.com>
2014-11-16 16:57:13 +0000

    Corrected Identation

--- src/main/scala/com/freq/core/Global.scala
+++ src/main/scala/com/freq/core/Global.scala
@@ -20,38 +20,36 @@ object Global extends App{
   val output = "Destination %s with Id %d:"
   println(s"Started with ids $ids" )
   executeWithFuture
-/**
- * Executes the word search wrapped in a Future
- */
- def executeWithFuture = {
-  val futures = ids.map{id=>Future(Tools.getDestInfo(id))}
-  futures.map( _ onComplete{
-    case Success(dest) => {
-      val res = getOrderedWordCount(Tools.getAllHotelDescriptions(dest).mkString)
-      if(res.length > 0){
-        showOutput(dest,res)
+  /**
+  * Executes the word search wrapped in a Future
+  */
+  def executeWithFuture = {
+    val futures = ids.map{id=>Future(Tools.getDestInfo(id))}
+    futures.map( _ onComplete{
+      case Success(dest) => {
+       val res = getOrderedWordCount(Tools.getAllHotelDescriptions(dest).mkString)
+       if(res.length > 0){ showOutput(dest,res) }
       }
-    }
-    case Failure (t) => println("Error thrown - %s".format(t.getClass().getSimpleName()))
+      case Failure (t) => println("Error thrown - %s".format(t.getClass().getSimpleName()))
     })
-}
-/**
- * Counts the word occurence for a given String, returning a sequence of tuples order by word count
- */
- private def getOrderedWordCount(text : String) : Seq[(String,Int)] = {
-  val words = new Regex(pttr,"g") findAllIn text
-  var res = collection.mutable.Map[String,Int]().withDefaultValue(0)
-
-  for(w<- words) {
-    res.update(w,(res(w)+1))
   }
-  res.toSeq.sortBy(_._2).reverse.toSeq
-}
+  /**
+   * Counts the word occurence for a given String, returning a sequence of tuples order by word count
+   */
+  private def getOrderedWordCount(text : String) : Seq[(String,Int)] = {
+    val words = new Regex(pttr,"g") findAllIn text
+    var res = collection.mutable.Map[String,Int]().withDefaultValue(0)
+
+    for(w<- words) {
+      res.update(w,(res(w)+1))
+    }
+    res.toSeq.sortBy(_._2).reverse.toSeq
+  }
 
 /**
  * Prints out the full output of one operation
  */
- private def showOutput(dest:Destination,seq:Seq[(String,Int)]){
+  private def showOutput(dest:Destination,seq:Seq[(String,Int)]){
     //To ensure the output is shown correctly
     this.synchronized {
       println(output.format(dest.name,dest.id))
@@ -62,7 +60,7 @@ object Global extends App{
 /**
  * Simple tools used in the main code
  */
- object Tools{
+object Tools{
   //URL to get information of destionation
   val destinationInfoUrl = "http://concierge.top10.com/v1/destinations/%s"
   val hotelInfoUrl = "http://concierge.top10.com/v1/destinations/%s/hotels?hotel_attrs=description&count=20"
